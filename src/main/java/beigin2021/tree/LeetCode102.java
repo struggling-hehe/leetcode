a5b36d0f1df7a58d193b415f7bc25f51225b1cff
clear <jiexipeng@kongfz.com>
2021-06-03 10:55:23 +0800

    add:层序遍历

--- src/main/java/beigin2021/tree/LeetCode102.java
+++ src/main/java/beigin2021/tree/LeetCode102.java
@@ -2,6 +2,7 @@ package beigin2021.tree;
 
 /**
  * 层序遍历
+ *
  * @date 2021/06/02
  */
 
@@ -13,50 +14,51 @@ import java.util.Queue;
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 public class LeetCode102 {
 
-    public List<List<Integer>> levelOrder(Node root) {
-        List<Integer> list = new LinkedList<Integer>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> li = new LinkedList<List<Integer>>();
-
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        if (root != null){
-            queue.add(root);
+        if (root == null) {
+            return li;
         }
+        List<Integer> list = new LinkedList<Integer>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
         TreeNode q = null;
-        while (!queue.isEmpty()){
+        TreeNode index = root;
+        while (!queue.isEmpty()) {
             TreeNode p = queue.poll();
-            if (p.left != null){
+            list.add(p.val);
+            if (p.left != null) {
                 queue.add(p.left);
                 q = p.left;
             }
-            if (p.right != null){
+            if (p.right != null) {
                 queue.add(p.right);
                 q = p.right;
             }
-            list.add(p);
-            if (p == q){
+
+            if (index == p) {
                 li.add(list);
-                list.clear();
+                list = new LinkedList<Integer>();
+                index = q;
             }
         }
-
+        return li;
     }
-    public static void main(String[] args) {
 
-    }
 }
 
 
